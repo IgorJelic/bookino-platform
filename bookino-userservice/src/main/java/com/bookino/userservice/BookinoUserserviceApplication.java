0178a32770a14e4713e6b8783c1b36b87ee9a777
IgorJelic <jelicigor.98@gmail.com>
2025-05-26 18:49:16 +0200

    my init commit

---
+++ bookino-userservice/src/main/java/com/bookino/userservice/BookinoUserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bookino.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookinoUserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookinoUserserviceApplication.class, args);
+	}
+
+}
---
+++ bookino-userservice/src/test/java/com/bookino/userservice/BookinoUserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookino.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookinoUserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
